017f7e0a0b77b207a34929869d779ef70be722d5
David olvera <doanaya@gmail.com>
2024-03-04 13:04:37 -0600

    Update comments

--- New_User_365.ps1
+++ New_User_365.ps1
@@ -1,15 +1,20 @@
-#Create New user
+#This script helps to create a new user with Office 365 bussines premium license
+# David Olvera V 1.0
 
+#Import Graph Module
 Import-Module Microsoft.Graph.Users
 
+#Connect to your tenant
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All"
+
+#Ask for new user details 
 $FirstName = Read-Host -Prompt "Please enter the First Name"
 $LastName = Read-Host -Prompt "Please enter the Last Name"
 $User = Read-Host -Prompt "Please enter the new Email"
 $Password = Read-Host -Prompt "Please enter the Password" -AsSecureString
 $Nick = Read-Host -Prompt "Please enter the Nick Name"
 
-
+#Create a Hash table the data 
 $params = @{
 	accountEnabled = $true
     mailNickname = $Nick
@@ -21,15 +26,16 @@ $params = @{
 	}
 }
 
+#Create new user with the hash table 
 New-MgUser -BodyParameter $params
 #Change Usage Location
 Update-MgUser -UserId $User -UsageLocation US
 
-#Get available licenses
+#Get  office 365 bussiness premium 
 $EmsSku = Get-MgSubscribedSku -All | Where-Object SkuPartNumber -eq 'O365_BUSINESS_PREMIUM'
 
 
-#Add License 
+#Add office 365 bussines premium  license  using SkuId
 
 Set-MgUserLicense -UserId $User  -AddLicenses @{SkuId = $EmsSku.SkuId} -RemoveLicenses @()
 
